95a133d56b807850b55fe04a4699429d28a9c55e
dbonafin <demianbonafina@gmail.com>
2022-08-29 16:30:07 +0200

    faker added

--- app/PackageModel.php
+++ app/PackageModel.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class PackageModel extends Model
 {
-    //
+    protected $table = 'packages';
 }
--- database/seeds/PackagesTableSeeder.php
+++ database/seeds/PackagesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use FakerGenerator\Faker;
+use App\PackageModel;
 
 class PackagesTableSeeder extends Seeder
 {
@@ -9,8 +11,18 @@ class PackagesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Faker $Faker)
+    {    
+        for($i = 1; $i < 4; $i++) {
+        // Create a new package
+        $new_package = new PackageModel();
+
+        $new_package->destination = $faker->city();
+        $new_package->nights = $faker->randomDigitNotNull();
+        $new_package->breakfast_included = $faker->boolean();
+        $new_package->price = $faker->randomFloat(2, 100, 999999);
+
+        $new_package->save();
+        }
     }
 }
